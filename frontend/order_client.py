cf3f5141996a2083172d823fb458214102f8ccf4
daoud010 <daoud010@gmail.com>
2024-01-10 22:12:09 -0500

    new function in order_client

--- frontend/order_client.py
+++ frontend/order_client.py
@@ -9,6 +9,21 @@ class OrderClient():
         self.port = 50051
 
         self.channel = grpc.insecure_channel(
-                        '{}:{}'.format(self.host, self.port)
+                        '{}:{}'.format(self.host, self.port))
         self.stub = service.OrderStub(self.channel)
-        )
+
+    def AddOrder(self, user_id, order_description, payment_type, payment_card_or_cash_details
+                 , order_amount):
+        order_info = message.OrderRequest(order_description=order_description, payment_type=payment_type,
+                                          payment_card_or_cash_details = payment_card_or_cash_details, order_amount=
+                                          order_amount, user_id=user_id)
+        return self.stub.AddOrder(order_info)
+
+    def EditOrder(self, order_description, payment_type, payment_card_or_cash_details, order_amount, user_id, order_id):
+        pass
+
+    def CancelOrder(self, user_id, order_id):
+        pass
+
+    def GetALlOrders(self, user_id):
+        pass
